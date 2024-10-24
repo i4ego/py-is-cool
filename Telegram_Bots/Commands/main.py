a1b1c84926022ca8b6810dc10b14cfc81d508a84
FeliBog <felibog@bk.ru>
2024-10-24 19:40:08 +0500

    Update main.py

--- Telegram_Bots/Commands/main.py
+++ Telegram_Bots/Commands/main.py
@@ -1,7 +1,7 @@
 import telebot # You need to download module telebot. Write in console "pip install telebot" or "pip3 install telebot"
 token = "" #Telegram bot token. You can create and get a token in bot "BotFather"
 bot = telebot.TeleBot(token)
-@bot.message_handler(commands=['hello'])
+@bot.message_handler(commands=['hello']) #you need to send to your bot command "/hello"
 def response(msg):
     bot.send_message(msg.chat.id, "Hello, world!")
 bot.infinity_polling()
