5edb0207a88f345ad30bfe598c39104523e61304
FeliBog <felibog@bk.ru>
2024-10-24 19:51:38 +0500

    Update main.py

--- Telegram_Bots/Echo-bot/main.py
+++ Telegram_Bots/Echo-bot/main.py
@@ -1,7 +1,7 @@
 import telebot # You need to download module telebot. Write in console "pip install telebot" or "pip3 install telebot"
 token = "" #Telegram bot token. You can create and get a token in bot "BotFather"
 bot = telebot.TeleBot(token)
-@bot.message_handler(content_types='text')
+@bot.message_handler(content_types='text') #send to your bot any text.
 def response(msg):
     bot.send_message(msg.chat.id, msg.text)
     print(f"{msg.chat.id}: {msg.text}")
