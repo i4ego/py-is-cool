5377108e72d03f7246a18a40974229e3ffca44ef
FeliBog <felibog@bk.ru>
2024-10-25 21:37:14 +0500

    Update main.py

--- API_examples/registration/main.py
+++ API_examples/registration/main.py
@@ -7,12 +7,15 @@ users = dict() #users
 class response(BaseModel): #type "response"
   name: str
   password: str
-@app.post('/reg') #registration
+@app.post('/reg') #registration (send POST request to http://127.0.0.1:8000/reg )
 async def reg(res: response):
   id = create_id(0, 100000)
   while id in users:
     id = create_id(0, 100000)
   users[id] = res
   return id
+@app.get('/') #userlist (send POST request to http://127.0.0.1:8000/ )
+async def getusers():
+    return users
 if __name__ == '__main__': #running API
   run(app)
